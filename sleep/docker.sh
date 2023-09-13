affcaebf31885e2bddbf35e4d8559ca3b36f17ee
sava <sava.stevanovic@pmf.edu.rs>
2023-09-13 08:51:09 +0200

    Init project

---
+++ sleep/docker.sh
@@ -0,0 +1,3 @@
+docker build -t pytorch_playground .
+xhost + 
+docker run --rm --name play -e DISPLAY=$DISPLAY --ipc=host --gpus all -p 6006:6006 -p 5009:5009 -dit -v `pwd`/project:/app -v /media/sava/Data/Data:/Data -v /tmp/.X11-unix:/tmp/.X11-unix  pytorch_playground
